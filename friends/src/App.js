849413442acabf3d4af847a45b2a77e2f3f1ce15
King Adkins <lambda_school_loaner_87@Lambda-School-Loaner-87s-MacBook-Pro.local>
2019-05-08 16:03:57 -0400

    Add some app data and build the components files

---
+++ friends/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import FriendsList from './components/FriendsList';
+import axios from 'axios';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/friends')
+      .then(response => {
+        console.log( response );
+        this.setState({ friends: response.data});
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+render() {
+  return (
+    <div className = 'App'>
+      <FriendsList friends = {this.state.friends}/>
+    </div>
+  );
+}
+
+export default App;
